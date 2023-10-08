0f1ba1c09085703ee452658e3bce1a792b025d2e
JavaBorka <borka34@gmail.com>
2023-10-08 21:36:41 +0200

    Úloha 5

--- src/Uloha5/index.jsx
+++ src/Uloha5/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './hamburger.css'
+import { useState } from 'react'
 
 // Zadání 1: Přichystej si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
 // Zadání 2: Po kliknutí na button změň stav z `true` na `false` nebo obráceně.
@@ -7,27 +8,40 @@ import './hamburger.css'
 // Zadání 4: Pokud je menu zavřené nech mu jen třídu `hamburger`. Pro otevřené přidej navíc `hamburger--otevrene`.
 
 export const Uloha5 = () => {
+	const [openMenu, setOpenMenu] = useState(false)
+
 	return (
 		<>
-			<button className="hamburger" aria-label="menu">
+			<button
+			onClick={() => {
+				setOpenMenu(!openMenu)
+			}}
+			className={openMenu ? "hamburger hamburger--otevrene" : "hamburger"}
+			aria-label="menu">
 				<span></span>
 				<span></span>
 				<span></span>
 			</button>
-			<ul>
-				<li>
-					<a href="#o-nas">O nás</a>
-				</li>
-				<li>
-					<a href="#co-delame">Co děláme</a>
-				</li>
-				<li>
-					<a href="#kontakty">Kontakty</a>
-				</li>
-				<li>
-					<a href="#cenik">Ceník</a>
-				</li>
-			</ul>
+				{openMenu && (
+					<ul>
+						<li>
+							<a href="#o-nas">O nás</a>
+						</li>
+						<li>
+							<a href="#co-delame">Co děláme</a>
+						</li>
+						<li>
+							<a href="#kontakty">Kontakty</a>
+						</li>
+						<li>
+							<a href="#cenik">Ceník</a>
+						</li>
+					</ul>
+				)}
 		</>
 	)
 }
+
+// **** POZNÁMKA PODMÍNĚNÉ ZOBRAZENÍ
+//Pokud nám jde skutečně jen o to, že se má na základě podmínky obsah zobrazit nebo nezobrazit, můžeme si zápis zjednodušit použitím operátoru && (logické AND, česky a zároveň).
+
