2d7f39bab3422d587c0820a03ceb0272cb3d16ad
JavaBorka <borka34@gmail.com>
2023-10-08 23:03:17 +0200

    Úloha 8 - moje riešenie

--- src/Uloha8/index.jsx
+++ src/Uloha8/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 // Zadání 1: Vytvoř stavovou proměnnou s výchozí hodnotou `'možná'`.
 // Zadání 2: Proměnnou vypiš místo `ano/ne/možná`.
 // Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 
 export const Uloha8 = () => {
+	const [odpoved, setOdpoved] = useState('možná')
+
 	return (
 		<>
-			<h3>Prší v Brně: ano/ne/možná</h3>
-			<button>změnit</button>
+			<h3>Prší v Brně: {odpoved}</h3>
+			<button onClick={() => {
+				setOdpoved(
+					odpoved === 'možná' ? 'ano' : (
+						odpoved === 'ano' ? 'ne' : 'možná'
+					) 						
+				)
+			}}
+			>
+				změnit
+			</button>
 		</>
 	)
 }
