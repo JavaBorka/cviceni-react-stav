e406223668fd42a3b599aef4c93dbf8f42c62b04
JavaBorka <borka34@gmail.com>
2023-10-08 23:06:14 +0200

    Úloha 8 - vzorové riešenie

--- src/Uloha8/index.jsx
+++ src/Uloha8/index.jsx
@@ -6,21 +6,21 @@ import { useState } from 'react'
 // Zadání 3: Po kliknutí na tlačítko změň `možná` na `ano`, `ano` na `ne`, případně `ne` na `možná`.
 
 export const Uloha8 = () => {
-	const [odpoved, setOdpoved] = useState('možná')
+	const [prsi, setPrsi] = useState('možná')
+	const zmen = () => {
+		if (prsi === 'možná') {
+			setPrsi('ano')
+		} else if (prsi === 'ano') {
+			setPrsi('ne')
+		} else {
+			setPrsi('možná')
+		}
+	}
 
 	return (
 		<>
-			<h3>Prší v Brně: {odpoved}</h3>
-			<button onClick={() => {
-				setOdpoved(
-					odpoved === 'možná' ? 'ano' : (
-						odpoved === 'ano' ? 'ne' : 'možná'
-					) 						
-				)
-			}}
-			>
-				změnit
-			</button>
+			<h3>Prší v Brně: {prsi}</h3>
+			<button onClick={zmen}>změnit</button>
 		</>
 	)
 }
