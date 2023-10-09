4cc99eb01b517c9d252a17e30d10e90401e2f014
JavaBorka <borka34@gmail.com>
2023-10-09 21:19:14 +0200

    Záverečný bonus 2

--- src/ZaverecnyBonus2/index.jsx
+++ src/ZaverecnyBonus2/index.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
 import './kasicka.css'
 import penizeUrl from './penize.svg'
+import { useState } from 'react'
 
 // Zadání 1: Přichystej v komponentě `Kasicka` proměnnou pro počet uložených peněz s výchozí hodnotou podle prop proměnné `pocatecniStav`.
 // Zadání 2: Prvním tlačítkem přidej pět a druhým nastav na nulu.
 // Zadání 3: Počet peněz vypiš.
 
 const Kasicka = ({ pocatecniStav }) => {
+	const [hodnota, setHodnota] = useState(pocatecniStav)
+
 	return (
 		<div className="kasicka">
 			<h3>
-				<img src={penizeUrl} width={24} height={24} alt="" /> Peníze v kasičce:{' '}
-				0 Kč
+				<img
+					src={penizeUrl}
+					width={24}
+					height={24}
+					alt=""
+				/> 
+					Peníze v kasičce: {hodnota} Kč
 			</h3>
-			<button>přihodit pětikorunu</button> <button>vysypat</button>
+			<button
+				onClick={() => {
+					setHodnota(hodnota+5)
+				}}
+			>
+				přihodit pětikorunu
+			</button>{' '}
+			<button 
+				onClick={() => {
+					setHodnota(0)
+			}}>
+				vysypat
+			</button>
 		</div>
 	)
 }
